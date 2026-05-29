1aa754114469456f820f1a79d3ce5abb8c466ca0
jacobsjkim-source <jacobsjkim@gmail.com>
2026-05-29 17:13:16 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export const metadata = {
   title: '오이코스 전도 프로그램',
   description: '소중한 한 영혼을 위한 30일 기도 여정',
@@ -8,13 +10,20 @@ export default function RootLayout({ children }) {
     <html lang="ko">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <meta name="theme-color" content="#534AB7" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+        <Script
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+          integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   )
 }
